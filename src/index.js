f03ecd4f0a1aec39477ef6189b152a6b3cbca55d
sungman323 <sungman.park@gmail.com>
2025-06-23 15:20:57 +0900

    250623 AuthContext

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { AuthProvider } from './contexts/AuthContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/project_3rd">
+    <BrowserRouter basename="/">
       <AuthProvider>
         <App />
       </AuthProvider>
